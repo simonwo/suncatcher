6418e4b46e6753c6d72f06dca0d98b250ef9fb67
Simon Worthington <simon@simonwo.net>
2025-04-07 17:37:35 +1000

    Only render when readings changed by ±250W every 30s.

--- render.sh
+++ render.sh
@@ -6,11 +6,47 @@ fmt() {
 	numfmt --to=si --field='-' --suffix=W --round=nearest $1
 }
 
-read -r POWER LOAD <<EOF
-$(sqlite3 -readonly -cmd '.mode tab' solar.db 'SELECT printf("%i", P_PV), printf("%i",ABS(P_LOAD)) from site ORDER BY rowid DESC LIMIT 1;')
+# At most update the display every 30s
+if [ -z "$(find display.png -mtime +0h0m30s)" ]; then
+	exit 0
+fi
+
+touch rowid.txt
+read -d ';' -r NEW_QUERY <<EOQ
+	SELECT rowid,
+		   printf("%i", P_PV) AS power,
+		   printf("%i", ABS(P_LOAD)) AS load
+	FROM site
+	ORDER BY Timestamp DESC
+	LIMIT 1;
+EOQ
+
+read -d ';' -r OLD_QUERY <<EOQ
+	SELECT rowid,
+	       printf("%i", P_PV) as power,
+		   printf("%i", ABS(P_LOAD)) AS load
+	FROM site
+	WHERE rowid = "$(cat rowid.txt)"
+	LIMIT 1;
+EOQ
+
+read -r NEW_ROWID NEW_POWER NEW_LOAD <<EOF
+$(sqlite3 -readonly -cmd '.mode tab' solar.db "$NEW_QUERY")
 EOF
 
-STYLESHEET=$(if [ $POWER -lt $LOAD ]; then echo "--stylesheet=bad.css"; else echo "--stylesheet=good.css"; fi)
+read -r OLD_ROWID OLD_POWER OLD_LOAD <<EOF
+$(sqlite3 -readonly -cmd '.mode tab' solar.db "$OLD_QUERY")
+EOF
 
-sed "s/POWER/$(fmt $POWER)/;s/LOAD/$(fmt $LOAD)/" display-template.svg |
-	rsvg-convert --width=250 --height=122 $STYLESHEET -b white >display.png
+THRESHOLD_WATTS=250
+if (($OLD_POWER < ($NEW_POWER - $THRESHOLD_WATTS))) ||
+   (($OLD_POWER > ($NEW_POWER + $THRESHOLD_WATTS))) ||
+   (($OLD_LOAD < ($NEW_LOAD - $THRESHOLD_WATTS))) ||
+   (($OLD_LOAD > ($NEW_LOAD + $THRESHOLD_WATTS))); then
+    STYLESHEET=$(if [ $NEW_POWER -lt $NEW_LOAD ]; then echo "--stylesheet=bad.css"; else echo "--stylesheet=good.css"; fi)
+
+	sed "s/POWER/$(fmt $NEW_POWER)/;s/LOAD/$(fmt $NEW_LOAD)/" display-template.svg |
+		rsvg-convert --width=250 --height=122 $STYLESHEET -b white >display.png
+
+	echo $NEW_ROWID > rowid.txt
+fi
